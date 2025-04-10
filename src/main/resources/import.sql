a8020c0140353b04e6f3afd745ab7bab64565746
DenielGuilhen <deniel.guilhen22@gmail.com>
2025-04-10 20:33:25 -0300

    BCrypt password

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$sxPBxs5Fp1AThvi3zSH.LOq..47//Gw.9IG0o8AEXvVZRA0n2MECO');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$sxPBxs5Fp1AThvi3zSH.LOq..47//Gw.9IG0o8AEXvVZRA0n2MECO');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
